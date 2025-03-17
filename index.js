3fd706dc7ae6f573d7035a871920bb17d2e8fe37
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-03-17 14:20:46 +0530

    Started Backend

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+require('dotenv').config()
+const express = require("express");
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Leo");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login on the Website</h1>");
+});
+
+app.get("/cat", (req, res) => {
+  res.send("<h2>Cats are seen here</h2>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example App listening on port ${port}`);
+});
